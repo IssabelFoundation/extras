e1c212e761db341815ac7b5ac2bf71cc89b1ad50
Nicolas Gudino <asternic@gmail.com>
2017-07-06 14:25:44 -0400

    Add Issabel Meet module and menu

---
+++ modules/meet/themes/default/js/javascript.js
@@ -0,0 +1,29 @@
+
+$(document).ready(function (){
+
+    if($('#emailcontent').length>0) {
+       $('#emailcontent').jqte();
+    }
+
+    if($('#meet').length>0) {
+        var height = $(window).height();
+        $('#meet').css('height', height * 0.9 | 0);
+        var domain = "meet.jit.si";
+        var roomid = $('#meet').data('room');
+        var room = roomid;
+        var username = $('#meet').data('username');
+        var width = '100%';
+        var height = 700;
+        var htmlElement = document.querySelector('#meet');
+        var configOverride = {
+            defaultLanguage: "es"
+        };
+        var interfaceConfigOverride = {
+            DEFAULT_REMOTE_DISPLAY_NAME: "Invitado"
+        };
+        var api = new JitsiMeetExternalAPI(domain, room, width, height, htmlElement, configOverride, interfaceConfigOverride);
+        api.executeCommand('displayName', username);
+        api.on("readyToClose",function() { console.log('ready to close'); window.location.href='index.php?menu=meet'; })
+    }
+
+});

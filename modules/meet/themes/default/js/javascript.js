0b5edfdd0fe24b466d2c69fb49ba6be351481e8c
Nicolas Gudino <asternic@gmail.com>
2017-11-10 08:42:22 -0500

    Fix Issabel Meet on Firefox, where api commands have to wait for conference to start

--- modules/meet/themes/default/js/javascript.js
+++ modules/meet/themes/default/js/javascript.js
@@ -7,13 +7,12 @@ $(document).ready(function (){
 
     if($('#meet').length>0) {
         var height = $(window).height();
-        $('#meet').css('height', height * 0.9 | 0);
+        height = height - 140;
         var domain = "meet.jit.si";
         var roomid = $('#meet').data('room');
         var room = roomid;
         var username = $('#meet').data('username');
         var width = '100%';
-        var height = 700;
         var htmlElement = document.querySelector('#meet');
         var configOverride = {
             defaultLanguage: "es"
@@ -22,8 +21,11 @@ $(document).ready(function (){
             DEFAULT_REMOTE_DISPLAY_NAME: "Invitado"
         };
         var api = new JitsiMeetExternalAPI(domain, room, width, height, htmlElement, configOverride, interfaceConfigOverride);
-        api.executeCommand('displayName', username);
         api.on("readyToClose",function() { window.location.href='index.php?menu=meet'; })
+        api.on("videoConferenceJoined",function() {
+            api.executeCommand('displayName', username);
+        });
+
     }
 
 });
